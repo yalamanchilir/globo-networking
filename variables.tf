68834d64e052340745f73936856cbe798fd2e07e
Ram Yalamanchili <ram.yalamanchili@slickor.com>
2025-06-26 19:42:17 -0500

    Add third subnet and tags

--- variables.tf
+++ variables.tf
@@ -20,6 +20,11 @@ variable "environment" {
   default     = "development"
 }
 
+variable "billing_code" {
+  type        = string
+  description = "(Required) Billing code for all resources"
+}
+
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
